baa90baa09a6113b7594857c9ebddbc93b9307dc
Vladimir A <vabizhan@inbox.ru>
2018-04-02 22:37:41 +0300

    Added a button and a print

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,14 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("hello World!")
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
